2dca07d19393395250f58cef1c36b5fb4c578855
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-06 23:17:31 +0000

    TO UPPERCASE

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) in range(ord('a'), ord('z') + 1):
+        return True
+    else:
+        return False
+
+def to_upper(c):
+    if islower(c):
+        return chr(ord(c) - 32)
+    return c
+
+
+def uppercase(str):
+    for c in str:
+        print("{}".format(to_upper(c)), end="")
+    print()
+
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
