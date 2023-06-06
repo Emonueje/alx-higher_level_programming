014912ec7f3fac1039380e3062b7223d5263cb87
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-06 22:50:02 +0000

    You more thinking and less material

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num in range(10):
+for num in range(0, 10):
     for n in range(num + 1, 10):
         if (num != 8 or n != 9):
             print("{}{},".format(num, n), end=" ")
