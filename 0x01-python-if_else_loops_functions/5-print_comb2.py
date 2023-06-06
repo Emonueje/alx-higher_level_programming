ba5373005a0ce050dfdbd2dcc1f0d042c3a8ccaf
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-06 21:28:55 +0000

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,7 @@ n = 0
 for number in range(0, 100):
     if number == 99:
         print(number)
-    elif number < n + 10 and number > n:
+    else:
         print("{}{},".format(int(n / 10), number % 10), end=" ")
     if number == n + 10:
         n += 10
