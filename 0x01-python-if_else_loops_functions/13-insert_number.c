d32dcb8caa074a946136c5a396539c8aba8ce8b5
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-08 23:55:52 +0000

    Nailed it

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -26,14 +26,17 @@ listint_t *insert_node(listint_t **head, int number)
 		(*head) = new_node;
 		return (new_node);
 	}
-	if ((*head)->n > number)
-	{
+	if ((*head)->n > number) /*check if first node data is greater than new node data */
+	{ /* if it is point new_node link to head pointer
+	     then point head to new =_node link */
 		new_node->next = (*head);
 		(*head) = new_node;
 		return (new_node);
 	}
-	while (current_node->next != NULL && current_node->next->n <= number)
+	while (current_node->next != NULL && current_node->next->n <= number) /* find fit position */
 		current_node = current_node->next;
+	/* point new_node to the node that contains a higher data
+	 * then point the current node link to new_node */
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 	return (new_node);
