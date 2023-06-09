e4be4efe765ff5a6f8be1cc4a7dfacc27392b9d3
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-09 00:38:22 +0000

    add

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    def good():
+        print("goof")
+
+
+    good()
+    return (a + b)
