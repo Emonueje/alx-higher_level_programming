db7b902c1086583caaff938771065f34240a2bec
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-21 13:32:20 +0000

    Simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key not in a_dictionary:
+        return a_dictionary
+    else:
+        new_dict = {}
+        for ki, val in a_dictionary.items():
+            if ki is not key:
+                new_dict[ki] = val
+        return new_dict
