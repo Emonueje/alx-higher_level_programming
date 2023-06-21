d67479e3b529084b1d336659bebd9f7bf354edb6
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-21 09:42:08 +0000

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,8 +2,3 @@
 def common_elements(set_1, set_2):
     new_set = set_1 & set_2
     return new_set
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
