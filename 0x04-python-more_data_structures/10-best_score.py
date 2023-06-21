ba85101925b2d64e76dc61d8e1a3a5a39393d746
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-21 15:00:01 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    bestscore = 0
+    best_score = 0
     if a_dictionary is None:
-        return  None
+        return None
     for key in a_dictionary:
-        if a_dictionary.get(key) > bestscore:
-            bestscore = a_dictionary.get(key)
+        if a_dictionary.get(key) > best_score:
+            best_score = a_dictionary.get(key)
     for ki in a_dictionary:
-        if a_dictionary.get(ki) == bestscore:
+        if a_dictionary.get(ki) == best_score:
             return ki
     return None
