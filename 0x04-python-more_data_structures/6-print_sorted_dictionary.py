7511e7de8681a3c52151e4dc69fc7d068d18cb05
Emmanuel Ebenezer <eebenezer949@gmail.com>
2023-06-21 10:19:57 +0000

    Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sort_dict = {key: a_dictionary[key] for key in sorted(a_dictionary)}
+    for i, j in sort_dict.items():
+        print("{}: {}".format(i , j))
