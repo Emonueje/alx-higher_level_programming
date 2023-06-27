4c7aac4a9876b7bf69316aa3d5c353c4d5b1b784
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-06-27 20:50:48 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that contains classes and functions for the classes and objects\
+        at alx"""
+
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size):
+        """initialization of the object"""
+        self.__size = size
