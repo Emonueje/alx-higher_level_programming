f7a3900710c5f24b0499e80fb43c4f06fdd4a6bc
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-06-27 03:39:47 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    number_printed = 0
-    i = 0
-    while (i < x):
-        try:
-            print(my_list[i], end='')
-            number_printed += 1
-        except as e:
-            break
-        i += 1
-    print()
-
-    return number_printed
+    if not my_list or x == 0:
+        print()
+        return (0)
+    try:
+        for num in range(x):
+            print(my_list[num], end="")
+    except:
+        num -= 1
+    finally:
+        print()
+        return(num + 1)
