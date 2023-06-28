b1ae4c1990d95466e47cc103f3df10ec897be26b
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-06-28 08:34:37 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,14 +28,29 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+    @property
+    def position(self):
+        """ Private attribute position getter """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """ Private position setter """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("Position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def my_print(self):
         """ prints to stdout the square with the character #"""
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
-                for k in range(self.__size):
-
-                    print("#", end="")
+            for i in range(self.__position[1]):
                 print()
+            for k in range(self.__size):
+                print("{}{}".format(" " * self.position[0], '#'*self.__size))
